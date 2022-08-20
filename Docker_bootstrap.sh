5a93e2cf2fb042963644028ecc91142b9928c1bd
Costas Kirgoussios <ckir1508@hotmail.com>
2022-08-20 20:08:53 +0300

    Initial test

--- Docker_bootstrap.sh
+++ Docker_bootstrap.sh
@@ -1,7 +1,8 @@
 me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-echo "Running $me"
+figlet -c -k Running $me
 
 # install google chrome
+figlet -c -k Installing Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get -y update
@@ -9,13 +10,17 @@ apt-get install -y google-chrome-stable
 rm -rf /var/lib/apt/lists/*
 
 # install python requirements
+figlet -c -k Installing Python
 pip install --upgrade pip
 pip install selenium webdriver-manager
 
 # install nodejs 
+figlet -c -k Installing Nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install node
 
+# Install application
+figlet -c -k Installing application
 mkdir /app
 cd /app
 # get HTTPWatchdog.js
@@ -24,10 +29,13 @@ curl -O https://raw.githubusercontent.com/ckir/httpwatchdog/main/HTTPWatchdog_lo
 chmod +x HTTPWatchdog_loop_run.sh
 ./HTTPWatchdog_loop_run.sh &
 echo "HTTPWatchdog_loop_run.sh started at port ${PORT:-9999}"
+figlet -c -k Started HTTPWatchdog
 
+# get Interceptor
 curl -O https://raw.githubusercontent.com/ckir/interceptor/main/app/interceptor.py
 curl -O https://raw.githubusercontent.com/ckir/interceptor/main/app/interceptor.js
-for (( ; ; ))
+figlet -c -k Starting Interceptor
+while true
 do
    python ./interceptor.py
    [ $? -eq 100 ] && break
