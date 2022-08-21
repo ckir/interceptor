eefb34f68fc4866742c55065de114069754f0bd9
Costas Kirgoussios <ckir1508@hotmail.com>
2022-08-21 21:47:26 +0300

    Initial test

--- Docker_bootstrap.sh
+++ Docker_bootstrap.sh
@@ -1,6 +1,11 @@
 me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-figlet -c -k Running $me
+# Declare the function
+function onexit() {
+  ./pyGSLogger.py --appname=${me} --loglevel=10 --lognotify=0 --logmessage="${me} Exited"
+}
+trap onexit exit SIGTERM SIGINT
 
+figlet -c -k Running $me
 mkdir /app
 cd /app
 
