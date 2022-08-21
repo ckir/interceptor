b7748162f20ab5ee2226816b5366ccf158d673bf
Costas Kirgoussios <ckir1508@hotmail.com>
2022-08-21 19:41:01 +0300

    Initial test

--- Docker_bootstrap.sh
+++ Docker_bootstrap.sh
@@ -36,8 +36,8 @@ chmod +x pyGSLogger.py
 figlet -c -k Installing Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get -qq -y update
-apt-get -qq install -y google-chrome-stable
+apt-get -qq -y update> /dev/null
+apt-get -qq install -y google-chrome-stable > /dev/null
 rm -rf /var/lib/apt/lists/*
 
 # get Interceptor
